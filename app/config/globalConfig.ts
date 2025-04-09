2ceb5cbbd1c7ed46a8004ea3b4525aae089afc71
euihyunee <lobil7782@gmail.com>
2025-04-09 15:43:09 +0900

    [chore] NextJS 프로젝트 폴더 구조 설정

---
+++ app/config/globalConfig.ts
@@ -0,0 +1,34 @@
+export const globalConfig = [
+  {
+    type: "income",
+    date: "2025-03-01",
+    price: 8000,
+    category: "용돈",
+    asset: "토스뱅크",
+    memo: "주.용돈",
+  },
+  {
+    type: "expend",
+    date: "2025-03-10",
+    price: 8000,
+    category: "편의점",
+    asset: "토스뱅크",
+    memo: "편의점 사용",
+  },
+  {
+    type: "income",
+    date: "2025-02-01",
+    price: 8000,
+    category: "용돈",
+    asset: "토스뱅크",
+    memo: "주.용돈",
+  },
+  {
+    type: "income",
+    date: "2025-01-01",
+    price: 8000,
+    category: "용돈",
+    asset: "토스뱅크",
+    memo: "주.용돈",
+  },
+];
