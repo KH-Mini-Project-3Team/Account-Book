9e35e1e7cf82cfc5aec81a2c0d5ad86c36c2a0bc
euihyunee <lobil7782@gmail.com>
2025-04-16 11:27:06 +0900

    [fix] 전역 layout.jsx DataContext.Provider 설정

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { DataProvider } from './contexts/DataContext';
 
 
 export default function RootLayout({children}){
   return (
     <html lang="en">
       <body>
-        {children}
+        <DataProvider>
+          {children}
+        </DataProvider>
       </body>
     </html>
   );
