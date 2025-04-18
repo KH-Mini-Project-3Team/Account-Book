df859cb042c8e8a3c53964885422f547d513e5ed
euihyunee <lobil7782@gmail.com>
2025-04-18 15:07:55 +0900

    [fix] Error 수정

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { DataProvider } from './contexts/DataContext';
-import './styles/globals.css';
-import styles from "../app/styles/layout.module.css"
-import { Dongle } from "next/font/google"
+import React from "react";
+import { DataProvider } from "./contexts/DataContext";
+import "./styles/globals.css";
+import styles from "../app/styles/layout.module.css";
+import { Dongle } from "next/font/google";
 import Header from "./components/header.jsx";
-import Navigation from "./components/navigation.jsx"
+import Navigation from "./components/navigation.jsx";
 
 const dongle = Dongle({
   subsets: ["latin"],
@@ -15,21 +15,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ko" className={dongle.className}>
       <body>
-        <div className={styles.container}>
-          <Header />
-          <main className={styles.mainContent}>
-            {children}
-          </main>
-          <nav className={styles.navigation}>
-            <Navigation />
-          </nav>
-        </div>
         <DataProvider>
+          <div className={styles.container}>
+            <Header />
+            <main className={styles.mainContent}>{children}</main>
+            <nav className={styles.navigation}>
+              <Navigation />
+            </nav>
+          </div>
+
           {children}
         </DataProvider>
-
       </body>
     </html>
   );
 }
-
