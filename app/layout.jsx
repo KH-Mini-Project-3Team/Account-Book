af61b61fdf79467f03b061405f6d645d3117810f
euihyunee <lobil7782@gmail.com>
2025-04-21 11:10:01 +0900

    [fix] root layout.jsx 중복 {children} 제거

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { DataProvider } from "./contexts/DataContext";
 import "./styles/globals.css";
-import styles from "../app/styles/layout.module.css";
 import { Dongle } from "next/font/google";
 import Header from "./components/header.jsx";
 import Navigation from "./components/navigation.jsx";
@@ -13,18 +12,16 @@ const dongle = Dongle({
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="ko" className={dongle.className}>
+    <html lang="ko">
       <body>
         <DataProvider>
-          <div className={styles.container}>
+          <div>
             <Header />
-            <main className={styles.mainContent}>{children}</main>
-            <nav className={styles.navigation}>
+            <main>{children}</main>
+            <nav>
               <Navigation />
             </nav>
           </div>
-
-          {children}
         </DataProvider>
       </body>
     </html>
