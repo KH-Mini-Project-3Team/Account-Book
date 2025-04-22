af368f4ef291d751508039f5beb4b5f83f5aee98
euihyunee <lobil7782@gmail.com>
2025-04-22 15:29:12 +0900

    [feat] globalConfig.ts 내용 DataContext에서 data list로 받아와 수정 가능하도록 변경 & InputAssets 페이지에서 id 파라미터로 수정, 삭제 화면, 없다면 신규 데이터 저장 화면으로 페이지 구현

--- app/contexts/DataContext.jsx
+++ app/contexts/DataContext.jsx
@@ -1,37 +1,70 @@
-'use client'; 
+"use client";
 
-import { createContext, useContext, useState } from 'react';
-import { globalConfig } from '../config/globalConfig'
+import { createContext, useContext, useState } from "react";
+import { globalConfig } from "../config/globalConfig";
 
 const DataContext = createContext();
 
-export function DataProvider({children}){
-
+export function DataProvider({ children }) {
   const [data, setData] = useState(globalConfig);
 
-  const addData = (item) => setData(prev => [...prev, item]);
-  // 예시: 데이터 수정 함수
-  const updateData = (idx, newItem) =>
-    setData(prev => prev.map((item, i) => (i === idx ? newItem : item)));
-  // 예시: 데이터 삭제 함수
-  const removeData = (idx) =>
-    setData(prev => prev.filter((_, i) => i !== idx));
+  // 데이터 수정
+  const updateItem = (id, newData) => {
+    setData(prev => prev.map(item => 
+      item.id === id ? { ...item, ...newData } : item
+    ));
+  };
 
-  const [revenuList, setRevenuList] = useState(["용돈", "금융소득", "월급"]);
-  const [expensesList, setExpensesList] = useState(["편의점", "카페", "음식점", "담배", "구독료"]);
-  const [assetList, setAssetList] = useState(["토스뱅크", "카카오뱅크", "농협", "IBK기업", "신한"])
+  // 데이터 삭제
+  const deleteItem = (id) => {
+    setData(prev => prev.filter(item => item.id !== id));
+  };
+
+  // 데이터 추가
+  const insertItem = (newItem) => {
+    const newId = Math.max(...data.map(item => item.id)) + 1;
+    setData(prev => [...prev, { ...newItem, id: newId }]);
+  };
+
+  const [revenuList, setRevenuList] = useState([
+    "용돈", 
+    "금융소득", 
+    "월급"]);
+  const [expensesList, setExpensesList] = useState([
+    "편의점",
+    "카페",
+    "음식점",
+    "담배",
+    "구독료",
+  ]);
+  const [assetList, setAssetList] = useState([
+    "토스뱅크",
+    "카카오뱅크",
+    "농협",
+    "IBK기업",
+    "신한",
+  ]);
 
   return (
-    <DataContext.Provider value={{
-      revenuList, setRevenuList,
-      expensesList, setExpensesList,
-      assetList, setAssetList
-    }}>
+    <DataContext.Provider
+      value={{
+        revenuList,
+        setRevenuList,
+        expensesList,
+        setExpensesList,
+        assetList,
+        setAssetList,
+        data,
+        updateItem,
+        deleteItem,
+        insertItem,
+      }}
+    >
       {children}
     </DataContext.Provider>
-  )
+  );
 }
 
-export function useData(){
+export function useData() {
   return useContext(DataContext);
 }
